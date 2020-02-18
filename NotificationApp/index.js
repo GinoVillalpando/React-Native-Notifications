83773b33785dcef832a54de7ede86a9b4e93d443
Gino Villalpando <grievxus@outlook.com>
2020-02-17 17:03:50 -0700

    added jquery and popper.js

--- NotificationApp/index.js
+++ NotificationApp/index.js
@@ -5,9 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 
 AppRegistry.registerComponent(appName, () => App);
